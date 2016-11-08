db344d047e2597333643ae7fd3078099ed5bd45e
Kristianengberg <krissoengberg@gmail.com>
2016-11-08 20:23:23 +0100

    first commit

---
+++ TcpAServer/TcpAServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+namespace TcpAServer
+{
+	public class TcpAServer
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Starting echo server...");
+
+			int port = 1234;
+			TcpListener listener = new TcpListener(IPAddress.Loopback, port);
+			listener.Start();
+
+			TcpClient client = listener.AcceptTcpClient();
+			NetworkStream stream = client.GetStream();
+			StreamWriter writer = new StreamWriter(stream, Encoding.ASCII) { AutoFlush = true };
+			StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+
+			while (true)
+			{
+				string inputLine = "";
+				while (inputLine != null)
+				{
+					inputLine = reader.ReadLine();
+					int count = 0;
+					foreach (char c in inputLine)
+						if (c == 'a') count++;
+					writer.WriteLine("Number of a's found: {0}", count);
+					Console.WriteLine("Number of a's found: {0}", count);
+				}
+				Console.WriteLine("Server saw disconnect from client.");
+			}
+		}
+	}
+}
